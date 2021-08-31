e14338217d1eb9eb5b9b015e5081f854f937f8e1
Lex Nelson <lexinelson597@gmail.com>
2021-08-31 11:54:33 -0400

    commit tree ced574586243f3e9cdbd13cd7cea29151f0eb071

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait
+
+    def clickbait
+        cb = ["Won't Believe", "Secret", "Top", "Guess"]
+        unless title.match? ("Won't Believe" )
+            errors.add(:title, 'must be clickbait')
+        end
+    end
 end
